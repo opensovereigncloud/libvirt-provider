0549955d5298b272924c64081034371f0f7d9d37
Sourav Sohan Sahu <108278794+so-sahu@users.noreply.github.com>
2024-08-20 16:01:31 +0530

    Extend Server Tests to Cover ListEvents (#370)

--- internal/server/server_suite_test.go
+++ internal/server/server_suite_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/ironcore-dev/ironcore/iri/remote/machine"
 	"github.com/ironcore-dev/libvirt-provider/api"
 	"github.com/ironcore-dev/libvirt-provider/cmd/libvirt-provider/app"
+	"github.com/ironcore-dev/libvirt-provider/internal/event/machineevent"
 	"github.com/ironcore-dev/libvirt-provider/internal/networkinterfaceplugin"
 	"github.com/ironcore-dev/libvirt-provider/internal/resources/sources"
 	. "github.com/onsi/ginkgo/v2"
@@ -43,6 +44,9 @@ const (
 	streamingAddress               = "127.0.0.1:20251"
 	healthCheckAddress             = "127.0.0.1:20252"
 	metricsAddress                 = "" // disable metrics server for integration tests
+	machineEventMaxEvents          = 10
+	machineEventTTL                = 10 * time.Second
+	machineEventResyncInterval     = 2 * time.Second
 )
 
 var (
@@ -130,6 +134,11 @@ var _ = BeforeSuite(func() {
 		ResyncIntervalGarbageCollector: resyncGarbageCollectorInterval,
 		ResyncIntervalVolumeSize:       resyncVolumeSizeInterval,
 		GuestAgent:                     app.GuestAgentOption(api.GuestAgentNone),
+		MachineEventStore: machineevent.EventStoreOptions{
+			MachineEventMaxEvents:      machineEventMaxEvents,
+			MachineEventTTL:            machineEventTTL,
+			MachineEventResyncInterval: machineEventResyncInterval,
+		},
 	}
 
 	srvCtx, cancel := context.WithCancel(context.Background())
