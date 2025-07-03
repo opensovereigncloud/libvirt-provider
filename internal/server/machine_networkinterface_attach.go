d474e0c8276134dd5a44801620534e3e753bf457
sync-token <project_238349_bot_7cf88cab2e4f35e877580caea2a275ba@noreply.gitlab.devops.telekom.de>
2025-07-03 07:23:48 +0000

    Improved `gRPC` error codes for `IRI` methods (#587)

--- internal/server/machine_networkinterface_attach.go
+++ internal/server/machine_networkinterface_attach.go
@@ -16,12 +16,12 @@ func (s *Server) AttachNetworkInterface(ctx context.Context, req *iri.AttachNetw
 	log.V(1).Info("Attaching NIC to machine")
 
 	if req == nil {
-		return nil, fmt.Errorf("AttachNetworkInterfaceRequest is nil")
+		return nil, convertInternalErrorToGRPC(fmt.Errorf("AttachNetworkInterfaceRequest is nil: %w", ErrInvalidRequest))
 	}
 
 	apiMachine, err := s.machineStore.Get(ctx, req.MachineId)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get machine: %w", err)
+		return nil, convertInternalErrorToGRPC(fmt.Errorf("failed to get machine '%s': %w", req.MachineId, err))
 	}
 
 	if api.GetExistingPCICount(apiMachine) >= apiMachine.Spec.PCIControllerTotal {
@@ -30,7 +30,7 @@ func (s *Server) AttachNetworkInterface(ctx context.Context, req *iri.AttachNetw
 
 	nicSpec, err := s.getNICFromIRINIC(req.NetworkInterface)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get nic from iri nic: %w", err)
+		return nil, convertInternalErrorToGRPC(fmt.Errorf("failed to get nic from iri nic: %w", err))
 	}
 
 	apiMachine.Spec.NetworkInterfaces = append(apiMachine.Spec.NetworkInterfaces, nicSpec)
